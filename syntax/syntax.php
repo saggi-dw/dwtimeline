5eddbcd57646e337e62407f4fa2408a4a1ad4161
saggi-dw <64777597+saggi-dw@users.noreply.github.com>
2022-06-28 16:16:49 +0200

    Surround unescaped output with paragraph
    Adding som comments
    Adding linefeed to output

--- syntax/syntax.php
+++ syntax/syntax.php
@@ -55,7 +55,7 @@ class syntax_plugin_dwtimeline_syntax extends \dokuwiki\Extension\SyntaxPlugin
         $data = [];
         switch ($state) {
             case DOKU_LEXER_ENTER :
-                $match = trim(substr($match, 11,-1));//
+                $match = trim(substr($match, 11,-1));// returns match between <dwtimeline(11) and >(-1)
                 $data = helper_plugin_dwtimeline_syntax::getTitleMatches($match, 'title');
                 return array($state,$data);
 
@@ -65,14 +65,14 @@ class syntax_plugin_dwtimeline_syntax extends \dokuwiki\Extension\SyntaxPlugin
                 $cnt = -1;
                 foreach ($milestones as $milestone) {
                     $cnt++;
-                    $entry = substr($milestone['entry'],10,-1);
+                    $entry = substr($milestone['entry'],10,-1);//returns match between <milestone(10) and >(-1)
                     $data = helper_plugin_dwtimeline_syntax::getEntryTitleMatches($entry, $cnt, $data);
                     $data['entries'][$cnt]['content'] = $milestone['content'];
                 }
                 return array($state,$data);
                 
             case DOKU_LEXER_EXIT :
-                $match = trim(substr($match, 12,-1));//
+                $match = trim(substr($match, 12,-1));//returns match between </dwtimeline(12) and >(-1)
                 $data = helper_plugin_dwtimeline_syntax::getTitleMatches($match, 'title');
                 return array($state,$data);
 
@@ -96,48 +96,47 @@ class syntax_plugin_dwtimeline_syntax extends \dokuwiki\Extension\SyntaxPlugin
             switch ($state) {
                 case DOKU_LEXER_ENTER :
                     //dbg('State Enter');
-                    $renderer->doc .= '<div class="timeline">';
+                    $renderer->doc .= '<div class="timeline">'. DOKU_LF;
                     if ($indata['title'] or $indata['description']) {
-                        $renderer->doc .= '<div class="container top">';
-                        $renderer->doc .= '<div class="content">';
-                        if ($indata['title']) {$renderer->doc .= '<h2>'.$indata['title'].'</h2>';}
-                        if ($indata['description']) {$renderer->doc .= $indata['description'];}
-                         $renderer->doc .= '</div>';
-                        $renderer->doc .= '</div>';
+                        $renderer->doc .= '<div class="container top">'. DOKU_LF;
+                        $renderer->doc .= '<div class="content">'. DOKU_LF;
+                        if ($indata['title']) {$renderer->doc .= '<h2>'.$indata['title'].'</h2>'. DOKU_LF;}
+                        if ($indata['description']) {$renderer->doc .= '<p>'.$indata['description'].'</p>'. DOKU_LF;}
+                         $renderer->doc .= '</div>'. DOKU_LF;
+                        $renderer->doc .= '</div>'. DOKU_LF;
                     }
                     break;
                 case DOKU_LEXER_UNMATCHED :
                     foreach($indata['entries'] as $entry)
                     {
-                        $renderer->doc .= '<div class="container '.$direction.'">';
-                        $renderer->doc .= '<div class="content">';
+                        $renderer->doc .= '<div class="container '.$direction.'">'. DOKU_LF;
+                        $renderer->doc .= '<div class="content">'. DOKU_LF;
                         if ($entry['title']) {
                             if ($entry['link']) {
-                               $renderer->doc .= '<h2>'.$this->render_text('[['.$entry['link'].'|'.$entry['title'].']]').'</h2>';
+                               $renderer->doc .= '<h2>'.$this->render_text('[['.$entry['link'].'|'.$entry['title'].']]').'</h2>'. DOKU_LF;
                             } else {
-                                $renderer->doc .= '<h2>'.$this->render_text($entry['title']).'</h2>';
+                                $renderer->doc .= '<h2>'.$this->render_text($entry['title']).'</h2>'. DOKU_LF;
                             }                           
                         }
-                        if ($entry['description']) {$renderer->doc .= '<h3>'.$entry['description'].'</h3>';}                
+                        if ($entry['description']) {$renderer->doc .= '<h3>'.$entry['description'].'</h3>'. DOKU_LF;}                
                         if ($entry['content']) {$renderer->doc .= $this->render_text($entry['content']);}
-                        $renderer->doc .= '</div>';
-                        $renderer->doc .= '</div>';
+                        $renderer->doc .= '</div>'. DOKU_LF;
+                        $renderer->doc .= '</div>'. DOKU_LF;
                         
                         $direction = helper_plugin_dwtimeline_syntax::getDirection($direction);
-
                     }
                     break;
                 case DOKU_LEXER_EXIT :
                     if ($indata['title'] or $indata['description']) {
-                        $renderer->doc .= '<div class="container bottom">';
-                        $renderer->doc .= '<div class="content">';
-                        if ($indata['title']) {$renderer->doc .= '<h2>'.$indata['title'].'</h2>';}
-                        if ($indata['description']) {$renderer->doc .= $indata['description'];}
-                        $renderer->doc .= '</div>';
-                        $renderer->doc .= '</div>';
+                        $renderer->doc .= '<div class="container bottom">'. DOKU_LF;
+                        $renderer->doc .= '<div class="content">'. DOKU_LF;
+                        if ($indata['title']) {$renderer->doc .= '<h2>'.$indata['title'].'</h2>'. DOKU_LF;}
+                        if ($indata['description']) {$renderer->doc .= '<p>'.$indata['description'].'</p>'. DOKU_LF;}
+                        $renderer->doc .= '</div>'. DOKU_LF;
+                        $renderer->doc .= '</div>'. DOKU_LF;
                     }
 
-                    $renderer->doc .= '</div>';        
+                    $renderer->doc .= '</div>'. DOKU_LF;        
                    
                     break;
             }
